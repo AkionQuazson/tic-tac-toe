559ae7bd9304e81ff8871826cf7dfbffec454b17
spencer <spencer@homelightarchive.com>
2022-12-13 20:28:27 -0700

    developed square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Square from './components/Square.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [gameSquares, setGameSquares] = useState(['','','','','','','','',''])
+	const [player, setPlayer] = useState(true);
+	return <div className="App">
+		<div className='containter'>
+			{gameSquares.map((v, i) => {
+				return <Square 
+				gameSquares={gameSquares}
+				setGameSquares={setGameSquares}
+				player={player}
+				setPlayer={setPlayer}
+				squareValue={v}
+				key={i}
+			/>
+			})}
+		</div>
+	</div>;
 }
 
 export default App;
